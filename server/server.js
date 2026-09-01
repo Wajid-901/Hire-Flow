60d28bda2e7d45ba0a355618d836ad048ba27cd5
Wajid-901 <abdulwajid8454@gmail.com>
2026-09-01 22:53:02 +0530

    fix: replace dotenvx with standard dotenv, clean server startup

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./config/database.js";
 
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDB();
-
-  app.listen(PORT, (req, res) => {
-    console.log(`Server is running on port ${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 };
+
 startServer();
